4f7c1d0a53599c542a8ad162ce6d2d57385498c2
Elham Bashari <elhambashari84@gmail.com>
2025-10-30 09:16:21 +0100

    server.ts created

--- srcServer/server.ts
+++ srcServer/server.ts
@@ -1,1 +1,42 @@
-console.log("paria")
+
+import express from "express";
+import cors from "cors";
+
+import users from "./routes/users.js";
+import channels from "./routes/channels.js";
+import messages from "./routes/messages.js";
+import login from "./auth/login.js";
+import register from "./auth/register.js";
+
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.use((req, res, next) => {
+  console.log(` ${req.method} ${req.url}`);
+  next();
+});
+
+
+app.use("/api/users", users);
+app.use("/api/channels", channels);
+app.use("/api/messages", messages);
+app.use("/api/auth/login", login);
+app.use("/api/auth/register", register);
+
+
+console.log("🔄 Server starting...");
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+
+
+
